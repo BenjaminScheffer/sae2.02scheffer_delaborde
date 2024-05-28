4da1753a4af966f92e8d69a9283701907acbb7f0
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-05-28 17:59:38 +0200

    premier test GrapheTest

--- test/GrapheTest.java
+++ test/GrapheTest.java
@@ -2,7 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class GrapheTest {
     @Test
-    public void test(){
-
+    public void testAjoutListe(){
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 2);
+        boolean b = g.listeNoeuds().contains("A");
+        assertEquals(true, b);
     }
 }
