20c8b6ec9e009e6e339cb1ae09d12792bec3fa0d
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-05-28 16:41:24 +0200

    ecriture debut GrapheListe

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
 public class GrapheListe implements  Graphe{
 
+    /** Attribut
+     * Liste de noeuds du graphe
+     */
+    private ArrayList<String> noeuds;
+
+    /** Attribut
+     * liste des arcs qui partent de chaque noeud du graphe
+     */
+    private ArrayList<Arcs> adjacence;
+
+    /**
+     * constructeur de la classe GrapheListe
+     */
+    public GrapheListe () {
+        this.noeuds = new ArrayList<>();
+        this.adjacence = new ArrayList<>();
+    }
+
+    /**
+     * Méthode qui permet d'ajouter un arc
+     * @param depart noeud de depart de l'arc
+     * @param destination noeud d'arriver de l'arc
+     * @param cout valeur de l'arc
+     */
+    public void ajouterArc(String depart, String destination, double cout) {
+        if ((noeuds.contains(depart)) && (noeuds.contains(depart))){
+            int indexDepart = noeuds.indexOf(depart);
+
+
+        }
+    }
 
 }
