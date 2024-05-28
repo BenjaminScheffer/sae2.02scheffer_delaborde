7cef6a577edf6c7239ebd4eb011bc799a48e2d66
Benjamin <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-05-28 17:12:11 +0200

    ecriture methode ajouterArc et getter

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -27,11 +27,39 @@ public class GrapheListe implements  Graphe{
      * @param cout valeur de l'arc
      */
     public void ajouterArc(String depart, String destination, double cout) {
-        if ((noeuds.contains(depart)) && (noeuds.contains(depart))){
-            int indexDepart = noeuds.indexOf(depart);
-
-
+        if (!noeuds.contains(depart)){
+            noeuds.add(depart);
         }
+        if (!noeuds.contains(destination)){
+            noeuds.add(destination);
+        }
+        int indexDepart = getIndice(depart);
+
+        if(this.adjacence.get(indexDepart)==null){
+            Arcs arc = new Arcs();
+            arc.ajouterArc(new Arc(destination,cout));
+            this.adjacence.add(indexDepart,arc);
+        }else{
+            Arcs list = this.adjacence.get(indexDepart);
+            int i = 0;
+            boolean arret = false;
+            while (!arret && i!=list.getArcs().size()){
+             if(list.getArcs().get(i).getDestination().equals(destination)){
+                 arret = true;
+             }
+             i++;
+            }
+            if(arret){
+                list.getArcs().get(i).setValeur(cout);
+            }else{
+                list.getArcs().add(new Arc(destination,cout));
+            }
+        }
+
+    }
+
+    public int getIndice(String n){
+        return this.noeuds.indexOf(n);
     }
 
 }
