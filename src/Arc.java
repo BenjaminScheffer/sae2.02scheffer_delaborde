dae5d81d5559cc1e211946db9038b04a1f042f2b
Benjamin <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-05-28 15:34:33 +0200

    Question 1 : Attributs + Constructeur + getter

---
+++ src/Arc.java
@@ -0,0 +1,33 @@
+public class Arc {
+    /** Attributs
+     * un attribut de destinations et de valeur.
+     */
+    private String destination;
+    private double valeur;
+
+    /**
+     * Constructeur qui initialise un Arc avec ça destination et la valeur de l'arc
+     * @param d
+     * @param v
+     */
+    public Arc(String d, double v){
+        this.destination = d;
+        this.valeur = v;
+    }
+
+    /**
+     * Getter de la destination
+     * @return
+     */
+    public String getDestination() {
+        return destination;
+    }
+
+    /**
+     * Getter de la valeur de l'arc
+     * @return
+     */
+    public double getValeur() {
+        return valeur;
+    }
+}
