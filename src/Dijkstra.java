1ed3bd0b184af3687f33722dfdcd6d423fd12ef1
scheffer <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-05-30 14:52:05 +0200

    ecriture methode resoudre

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,27 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Dijkstra {
-    public Valeur resoudre(Graphe g, String depart){
+    public static Valeur resoudre(Graphe g, String depart){
         //création de la valeur
-        List Q= new ArrayList();
+        List<String> Q= new ArrayList<String>();
         Valeur val = new Valeur();
         //Initialisation de toutes les valeurs donc : 0 pour le String n et =l'infini pour le reste
         for(int i = 0;i<g.listeNoeuds().size();i++){
-            //Q[i] = g.listeNoeuds().get(i);
-            if(g.listeNoeuds().get(i).equals(depart)){
-                val.setValeur(depart,0);
-            }else{
-                val.setValeur(g.listeNoeuds().get(i),Double.MAX_VALUE);
+            val.setValeur(g.listeNoeuds().get(i),Double.MAX_VALUE);
+            Q.add(g.listeNoeuds().get(i));
+        }
+        val.setValeur(depart,0);
+        while(!Q.isEmpty()){
+            double min = Double.MAX_VALUE;
+            int indice = 0;
+            for(int i = 0; i < Q.size();i++){
+                if(val.getValeur(Q.get(i))<min){
+                    indice = i;
+                }
+            }
+            String u = Q.get(indice);
+            Q.remove(indice);
+            for(int i = 0;i<Q.size();i++){
+                for(int j = 0 ; j<g.suivants(u).size();j++){
+                    double d = val.getValeur(u) + g.suivants(u).get(j).getValeur() ;
+                    if(d<val.getValeur(g.suivants(u).get(j).getDestination())){
+                        val.setValeur(g.suivants(u).get(j).getDestination(),d);
+                        val.setParent(g.suivants(u).get(j).getDestination(),u);
+                    }
+                }
             }
         }
-        while(!Q.isEmpty()){
-
-        }
-
-
-
-
         return val;
     }
 
