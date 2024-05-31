60b41adac0b0b55a46b146ff48ffe808c398dc30
Benjamin <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-05-31 08:50:03 +0200

    Suppression du main ajout d'un affichage plus jolie dans bellemanford et ecriture main Dijkstra

--- src/MainDijkstra.java
+++ src/MainDijkstra.java
@@ -1,4 +1,4 @@
-public class MainBellmanFord {
+public class MainDijkstra {
     public static void main(String[] args) throws Exception {
         GrapheListe graphe = new GrapheListe();
         graphe.ajouterArc("D", "B", 23);
@@ -11,13 +11,13 @@ public class MainBellmanFord {
         System.out.println("Graphe : ");
         System.out.println(graphe.toString());
         System.out.println("*****************************");
-        System.out.println("Bellmanford en partant de D");
-        System.out.println(BellmanFord.pointfixe(graphe, "D"));
+        System.out.println("Dikstra en partant de D");
+        System.out.println(Dijkstra.resoudre(graphe, "D"));
         System.out.println("Chemin pour aller a E");
-        System.out.println(BellmanFord.pointfixe(graphe, "D").calculerChemin("E"));
-        System.out.println("BellmanFord en partant de A");
-        System.out.println(BellmanFord.pointfixe(graphe, "A"));
+        System.out.println(Dijkstra.resoudre(graphe, "D").calculerChemin("E"));
+        System.out.println("Dikstra en partant de A");
+        System.out.println(Dijkstra.resoudre(graphe, "A"));
         System.out.println("Chemin pour aller a C");
-        System.out.println(BellmanFord.pointfixe(graphe, "A").calculerChemin("C"));
+        System.out.println(Dijkstra.resoudre(graphe, "A").calculerChemin("C"));
     }
 }
