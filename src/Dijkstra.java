d4cbc8cd9ec08d469e4ab837ae9a3bf8e21162bc
Benjamin <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-05-31 10:14:00 +0200

    tentative de modification

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -36,12 +36,14 @@ Fin
         //Initialisation de toutes les valeurs donc : 0 pour le String n et =l'infini pour le reste
         for(int i = 0;i<g.listeNoeuds().size();i++){
             val.setValeur(g.listeNoeuds().get(i),Double.MAX_VALUE);
+            val.setParent(g.listeNoeuds().get(i),null);
             Q.add(g.listeNoeuds().get(i));
         }
         val.setValeur(depart,0);
         while(!Q.isEmpty()){
             double min = Double.MAX_VALUE;
             int indice = 0;
+
             for(int i = 0; i < Q.size();i++){
                 if(val.getValeur(Q.get(i))<min){
                     indice = i;
@@ -49,6 +51,9 @@ Fin
             }
             String u = Q.get(indice);
             Q.remove(indice);
+            if(g.suivants(u)==null){
+                return val;
+            }
             for(int i = 0;i<Q.size();i++){
                 for(int j = 0 ; j<g.suivants(u).size();j++){
                     double d = val.getValeur(u) + g.suivants(u).get(j).getValeur() ;
