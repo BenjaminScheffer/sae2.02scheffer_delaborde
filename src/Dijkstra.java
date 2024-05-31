6c713776f9c6d2bd785dfb763dcab84c88e2f9cb
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-05-31 08:32:20 +0200

    ajout algo en commentaire classe dijkstra

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,6 +1,34 @@
 import java.util.*;
 
 public class Dijkstra {
+    /*
+    * Entrées :
+G un graphe orient´e avec une pond´eration positive des arcs (co^ut ou poids)
+A un sommet (d´epart) de G
+
+Début
+    Q <- {} // utilisation d’une liste de noeuds `a traiter
+    Pour chaque sommet v de G faire
+        v.valeur <- Infini
+        v.parent <- Ind´efini
+        Q <- Q U {v} // ajouter le sommet v `a la liste Q
+    Fin Pour
+    A.valeur <- 0
+    Tant que Q est un ensemble non vide faire
+        u <- un sommet de Q telle que u.valeur est minimal
+        // enlever le sommet u de la liste Q
+        Q <- Q \ {u}
+        Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
+            d <- u.valeur + poids(u,v)
+            Si d < v.valeur
+                // le chemin est plus interessant
+                Alors v.valeur <- d
+                    v.parent <- u
+            Fin Si
+        Fin Pour
+    Fin Tant que
+Fin
+* */
     public static Valeur resoudre(Graphe g, String depart){
         //création de la valeur
         List<String> Q= new ArrayList<String>();
